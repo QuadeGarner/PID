830860f8e04c92f25e440b02e05270cad88dfb1e
Quade Garner <qmgarner@iastate.edu>
2026-04-30 01:12:30 -0500

    Added kI variable and added graph

--- src/main/java/Simulator/TelemetryDashboard.java
+++ src/main/java/Simulator/TelemetryDashboard.java
@@ -8,26 +8,46 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polyline;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class TelemetryDashboard extends Application {
     VirtualMotor vm = new VirtualMotor();
-    private double scale  = 0;
-    private double offset = 0;
+//    private double scale  = 0;
+//    private double offset = 0;
     private double target;
-    private double kP = 0.01; // Gain - sensitivity of the motor
-    private double kD = 0.1;
+    private double kP = 0.008; // Gain - sensitivity of the motor
+    private double kD = 0.03; //
+    private double kI = 0.0001;
     private double lastError = 500;
     double currentPos;
     double error;
     double derivative;
     double power;
+    double intergalSum = 0;
+    double timeX = 0;
     @Override
     public void start(Stage primaryStage){
         Pane root = new Pane();
-        Scene scene = new Scene(root, 800, 600);
+        Pane graph = new Pane();
+        Polyline targetLine = new Polyline();
+        targetLine.setLayoutX(50);
+        targetLine.setStroke(Color.GREEN);
+        targetLine.setStrokeWidth(2);
+        Polyline motorLine = new Polyline();
+        motorLine.setLayoutX(50);
+        motorLine.setStroke(Color.YELLOW);
+        motorLine.setStrokeWidth(2);
+        Polyline powerLine = new Polyline();
+        powerLine.setStrokeWidth(2);
+        powerLine.setStroke(Color.WHITE);
+        powerLine.setLayoutX(50);
+        Rectangle rect = new Rectangle(800,400, Color.LIGHTGRAY);
+        rect.setLayoutX(50);
+        Scene scene = new Scene(root, 1200, 600);
         Circle robot = new Circle(10, Color.BLUE);
-        robot.setCenterY(300);
+        robot.setCenterY(450);
         robot.setCenterX(50);
         root.getChildren().add(robot);
         Slider slider = new Slider();
@@ -39,16 +59,30 @@ public class TelemetryDashboard extends Application {
         slider.setPrefWidth(500);
         target = slider.getValue();
         root.getChildren().add(slider);
-        TextArea ta = new TextArea();
-        root.getChildren().add(ta);
+        root.getChildren().add(graph);
+        root.getChildren().add(rect);
+        root.getChildren().add(motorLine);
+        root.getChildren().add(targetLine);
+        root.getChildren().add(powerLine);
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
+                timeX += 0.5;
+                if(target != slider.getValue()){
+                    intergalSum = 0;
+                }
                 setTarget(slider.getValue());
                 currentPos = vm.getPosition();
                 error = target - currentPos;
+                intergalSum += error;
+                if(intergalSum > 100){
+                    intergalSum = 100;
+                }
+                if(intergalSum < -100) {
+                    intergalSum = -100;
+                }
                 derivative = error - lastError;
-                power = (error * kP) + (derivative * kD);
+                power = (error * kP)+ (kI * intergalSum) + (derivative * kD);
                 lastError = error;
 
                 if(power > 1.0 ){
@@ -57,23 +91,37 @@ public class TelemetryDashboard extends Application {
                 if(power < -1.0){
                     power = -1.0;
                 }
-                vm.update(power);
-                robot.setCenterX(vm.getPosition());
+                if( power < 0.05 && power > 0) {
+                    power = 0.05;
+                }else if( power > -0.05 && power < 0){
+                    power = -.05;
+                }
+
+                double posMap = (currentPos - 100) * (200.0 / 500.0);
+                double targetMap = (target - 100) * (200.0 / 500.0);
+                motorLine.getPoints().addAll(timeX, 200 - posMap);
+                targetLine.getPoints().addAll(timeX, 200 - targetMap);
+                powerLine.getPoints().addAll(timeX, 300- (power *80));
+
+                if(timeX > 800){
+                    motorLine.getPoints().clear();
+                    targetLine.getPoints().clear();
+                    powerLine.getPoints().clear();
+                    timeX = 0;
+                }
+
 
                 if(Math.abs(error) < 0.1 && Math.abs(vm.getVelocity())< 0.1){
                     return;
                 }
-                String values = "Pos: %.2f"+currentPos +" | Error: %.2f "+ error +"| Power: %.2f%n" + power;
-                ta.setText(values);
+                vm.update(power);
+                robot.setCenterX(vm.getPosition());
             }
         };
         primaryStage.setTitle("Motor Firmware Telemetry");
         primaryStage.setScene(scene);
         primaryStage.show();
         timer.start();
-    }
-    private double scaleFactor(){
-        return (vm.getPosition() * scale) + offset;
     }
     public static void main(String[] args) {
         launch(args);
