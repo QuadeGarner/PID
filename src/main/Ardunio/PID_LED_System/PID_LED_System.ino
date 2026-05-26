51d806f05853797b09020e3c36f3b4eef1039300
Quade Garner <qmgarner@iastate.edu>
2026-05-25 23:14:50 -0500

    Add IntegralSum to the PD to make system PID

--- src/main/Ardunio/PID_LED_System/PID_LED_System.ino
+++ src/main/Ardunio/PID_LED_System/PID_LED_System.ino
@@ -15,13 +15,13 @@ long delta_t = 0;
 double dd_t;
 double baseOffset = 100;
 double safeOffset =1;
-char inputArr[64];
-double  field1;
+double field1;
 double field2;
+double field3;
 char field1Arr[32];
 char field2Arr[32];
-int i =0;
-int j = 0;
+char field3Arr[32];
+int i =0, j = 0 ,k = 0;
 FieldLoadingState state = FieldLoadingState::FIELD1;
 CurrentState currState = CurrentState:: WAITING_FOR_DATA;
 
@@ -40,50 +40,65 @@ void loop() {
   // put your main code here, to run repeatedly:
   int potVal = analogRead(potPin);
 
-  while(Serial.available() && state != CLEARDATA){
+  while(Serial.available() && state != FieldLoadingState::CLEARDATA){
     char c = Serial.read();
-    if( i > 30 || j >30 ){
+    if( i > 30 || j >30 || k > 30){
       currState = CurrentState:: PACKET_ERROR;
       field1 = 0;
       field2 = 0;
+      field3 = 0;
       i = 0;
       j =0;
+      k = 0;
       currState = CurrentState:: RECOVERY;
     }
-    if(c == ','){
-      currState = CurrentState:: READING_DATA_FOR_FIELD3;
-      state= FieldLoadingState::FIELD3
+    if(c == ',' && currState == CurrentState::READING_DATA_FOR_FIELD1 ){
+      currState = CurrentState:: READING_DATA_FOR_FIELD2;
+      state= FieldLoadingState::FIELD2;
       field1Arr[i] = '\0';
+    }else if (c == ',' && currState == CurrentState::READING_DATA_FOR_FIELD2 ){
+      currState = CurrentState:: READING_DATA_FOR_FIELD3;
+      state = FieldLoadingState::FIELD3;
+      field2Arr[j] = '\0';
     }
     else if(c != '\n' && state == FieldLoadingState::FIELD1){
       currState = CurrentState::READING_DATA_FOR_FIELD1;
       field1Arr[i] = c;
       i++;
-    }else if(c != '\n' &&  state == FieldLoadingState::FIELD3){
+    }else if(c != '\n' &&  state == FieldLoadingState::FIELD2){
+      currState = CurrentState::READING_DATA_FOR_FIELD2;
       field2Arr[j] = c;
       j++;
+    }else if( c != '\n' &&  state == FieldLoadingState::FIELD3){
+      currState = CurrentState::READING_DATA_FOR_FIELD3;
+      field3Arr[k] = c;
+      k++;
     }
     else{
       currState = CurrentState::PACKET_COMPLETE;
       state = FieldLoadingState::CLEARDATA;
-      field2Arr[j] = '\0';
+      field3Arr[j] = '\0';
     }
 }
   field1 = atof(field1Arr);
   field2 = atof(field2Arr);
+  field3 = atof(field3Arr);
   controller.setTarget((double)map(potVal, 0, 4095, 101, 600));
-  if(state == state::CLEARDATA){
+  if(state == FieldLoadingState::CLEARDATA){
     controller.setKp(field1);
-    controller.setKd(field2);
+    controller.setKI(field2);
+    controller.setKd(field3);
     field1 = 0;
     field2 = 0;
-    clearData = 0;
+    field3 = 0;
     state= FieldLoadingState::FIELD1;
     currState = CurrentState:: WAITING_FOR_DATA;
     i = 0;
     j = 0;
+    k = 0;
     std::fill(std::begin(field1Arr), std::end(field1Arr), NULL);
     std::fill(std::begin(field2Arr), std::end(field2Arr), NULL);
+    std::fill(std::begin(field3Arr), std::end(field3Arr), NULL);
   }
   currentTime = millis();
   delta_t = currentTime - lastTime;
@@ -103,7 +118,7 @@ void loop() {
   }
   percentComplete =  fabs(percentComplete);
   Serial.print(String(controller.getTarget()) +","+String(controller.computeError()) +","+
-  String(controller.getLastError())+","+String(vm.getPosition())+","+String(controller.getKp())+","
+  String(controller.getLastError())+","+String(vm.getPosition())+","+String(controller.getKp())+","+ String(controller.getKi())+","
   +String(controller.getKd())+","+String(percentComplete)+","+String(controller.getPower()) +"\n");
   if( percentComplete <= 50){
       // Left redLight
